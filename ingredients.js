e0705035471b19c2bdf85152c56c91289a9466a6
Alexandra Scharfenberger <ascharfe55@gmail.com>
2021-12-26 20:20:16 -0700

    banana bread question

---
+++ ingredients.js
@@ -0,0 +1,13 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+//let i = 0;
+
+//while (i < ingredients.length) {
+//	i++;
+//};
+
+//for (i = 0; i < ingredients.length; i++);
+	
+ingredients.reverse();
+
+console.log(ingredients);
+
